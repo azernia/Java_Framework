eded75d7f0f06dcc02e211eefd569de6f5f3e4b9
rui <872715188@qq.com>
2021-01-29 11:03:39 +0800

    Spring与MyBatis整合,使Tomcat启动时加载applicationContext.xml

--- 13-Spring-02/src/com/rui/servlet/AirportServlet.java
+++ 13-Spring-02/src/com/rui/servlet/AirportServlet.java
@@ -13,20 +13,22 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import com.rui.service.AirportService;
+import com.rui.service.impl.AirportServiceImpl;
 
-@WebServlet("/AirportServlet")
+@WebServlet("/as")
 public class AirportServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private AirportService as;
 
 	@Override
-	public void init(ServletConfig config) throws ServletException {
+	public void init() throws ServletException {
 		ApplicationContext ac = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
-		as = ac.getBean("airportService", AirportService.class);
+		as = ac.getBean("airportService", AirportServiceImpl.class);
 	}
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+		req.setAttribute("list", as.showAll());
+		req.getRequestDispatcher("index.jsp").forward(req, resp);
 	}
 }
